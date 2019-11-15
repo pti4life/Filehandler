a437e4f9d1c13e99c02d67278a34b99a7e66d340
pti4life <kallai.roland@outlook.com>
2019-11-15 19:51:59 +0100

    Userhez tartozó Data Transfer Objet módosítása

--- Application/src/main/java/com/csaszarteam/fileuploader/service/domain/UserDTO.java
+++ Application/src/main/java/com/csaszarteam/fileuploader/service/domain/UserDTO.java
@@ -1,44 +1,39 @@
 package com.csaszarteam.fileuploader.service.domain;
 
-import java.util.Set;
-
 
 public class UserDTO {
 
-    private Long id;
+    private String name;
+
+    private String email;
 
     private String username;
 
     private String password;
 
-    private boolean enabled;
-
-    private Set<UserRoleDTO> userRoles;
-
-    public Set<UserRoleDTO> getUserRoles() {
-        return userRoles;
-    }
-
     public UserDTO(){}
 
-    public UserDTO(Long id, String username, String password, boolean enabled, Set<UserRoleDTO> userRoles){
+    public UserDTO(String name, String email, String username, String password){
+        this.name=name;
+        this.email=email;
         this.username = username;
         this.password = password;
-        this.enabled = enabled;
-        this.id=id;
-        this.userRoles = userRoles;
     }
 
-    public void setUserRoles(Set<UserRoleDTO> userRoles) {
-        this.userRoles = userRoles;
+    public String getName() {
+        return name;
     }
 
-    public boolean isEnabled() {
-        return enabled;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getUsername() {
@@ -57,18 +52,11 @@ public class UserDTO {
         this.password = password;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     @Override
     public String toString() {
-        return "user dto{" +
-                "id=" + id +
+        return "users{" +
+                " name="+name+
+                ", email="+email+
                 ", username='" + username + '\'' +
                 ", password='" + password + '\'' +
                 '}';
