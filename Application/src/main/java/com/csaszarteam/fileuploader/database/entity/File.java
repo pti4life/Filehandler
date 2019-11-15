37d074ce8caa01b16030b737cc548bf22faa7543
pti4life <kallai.roland@outlook.com>
2019-11-15 19:46:53 +0100

    File entitás hozzáadása

---
+++ Application/src/main/java/com/csaszarteam/fileuploader/database/entity/File.java
@@ -0,0 +1,25 @@
+package com.csaszarteam.fileuploader.database.entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name="files")
+public class File {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String fileName;
+
+    private long fileSize;
+
+    private String mimeType;
+
+    @JoinColumn(name ="user_id")
+    private long senderID;
+
+    @ManyToOne
+    private User user;
+
+}
