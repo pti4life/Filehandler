8cf7ec29ea49dfdaadc65c9d10e2c035cd6e276e
pti4life <kallai.roland@outlook.com>
2019-11-16 02:05:51 +0100

    StaticPageController létrehozása az egyéb statikus oldalak kezelésére, jelenleg csak a home.jsp-t kezeli

---
+++ Application/src/main/java/com/csaszarteam/fileuploader/controller/StaticPageController.java
@@ -0,0 +1,13 @@
+package com.csaszarteam.fileuploader.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StaticPageController {
+    @RequestMapping("/")
+    public String showSignupPage(Model model) {
+        return "home";
+    }
+}
