9f9a11ed82ca022d13b3519d7205dac542286d27
hidie <hidieric@gmail.com>
2019-12-08 16:38:35 +0100

    master: first test method added

---
+++ Application/src/test/com/csaszarteam/fileuploader/database/entity/UserRoleTest.java
@@ -0,0 +1,17 @@
+package com.csaszarteam.fileuploader.database.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRoleTest {
+
+    UserRole userRole = new UserRole();
+
+    @Test
+    void getId() {
+        userRole.setId(150);
+
+        assertEquals(150, userRole.getId());
+    }
+}
