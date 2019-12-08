5c45b80034b4d1eb85338616a4d0d645b6c10cc9
hidie <hidieric@gmail.com>
2019-12-08 16:27:14 +0100

    master: Test directory generated

---
+++ Application/src/test/com/csaszarteam/fileuploader/controller/ProfileControllerTest.java
@@ -0,0 +1,24 @@
+package com.csaszarteam.fileuploader.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfileControllerTest {
+
+    @Test
+    void showProfilePage() {
+    }
+
+    @Test
+    void updateEmail() {
+    }
+
+    @Test
+    void updateUserName() {
+    }
+
+    @Test
+    void updatePassword() {
+    }
+}
